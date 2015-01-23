827158db2594826ac4b04124c10142738952412d
user <user@asu2>
2015-01-23 14:41:19 -0600

    add apache math code for javadoc

--- src/solarpositioning/SPATestloop.java
+++ src/solarpositioning/SPATestloop.java
@@ -4,9 +4,11 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.SimpleTimeZone;
 
+import org.apache.commons.math3.geometry.euclidean.threed.SphericalCoordinates;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
 import solarpositioning.AzimuthZenithAngle;
 import solarpositioning.SPA;
-
 
 public class SPATestloop {
 
@@ -14,8 +16,13 @@ public class SPATestloop {
 
 		double lat = 30.25;
 		double az, alt;
+		double azRad, altRad;
 		double dec, HA;
-		String s = String.format("%-12s%-12s%-12s%-12s%-12s", "hour", "az", "alt", "dec", "HA");
+		String t;
+
+		// String s = String.format("%-12s%-12s%-12s%-12s%-12s", "hour", "az",
+		// "alt", "dec", "HA");
+		String s = String.format("%-12s%-12s%-12s%-12s%-12s%-12s", "hour", "az", "alt", "x", "y", "z");
 		System.out.println(s);
 
 		for (int hour = 1; hour < 25; hour++) {
@@ -27,9 +34,22 @@ public class SPATestloop {
 			alt = 90 - result.getZenithAngle();
 			dec = Math.asin(Math.sin(alt) * Math.sin(lat) + Math.cos(alt) * Math.cos(lat) * Math.cos(az));
 			HA = Math.acos((Math.sin(alt) - Math.sin(lat) * Math.sin(dec)) / Math.cos(lat) * Math.cos(dec));
-			//HA = 0.7;
-			String t = String.format("%-12d%-12.5f%-12.5f%-12.5f%-12.5f", hour, az, alt, dec, HA);
+			// HA = 0.7;
+			// String t = String.format("%-12d%-12.5f%-12.5f%-12.5f%-12.5f",
+			// hour, az, alt, dec, HA);
+			// System.out.println(t);
+
+			azRad = org.apache.commons.math3.util.FastMath.toRadians(az);
+			altRad = org.apache.commons.math3.util.FastMath.toRadians(alt);
+			SphericalCoordinates rs = new SphericalCoordinates(1, azRad, altRad);
+			Vector3D r = rs.getCartesian();
+			double x = r.getX();
+			double y = r.getY();
+			double z = r.getZ();
+
+			t = String.format("%-12d%-12.5f%-12.5f%-12.5f%-12.5f%-12.5f", hour, az, alt, x, y, z);
 			System.out.println(t);
+
 		}
 	}
 
