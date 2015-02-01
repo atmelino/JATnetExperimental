6835f679b7710d19ccae212117b92a3b1d171053
jatnik <jatnik@jat>
2015-01-31 20:48:07 -0600

    fix time zone issue sun position

--- src/jat/examples/coordinates/comparison/JAT_SPA_compare01.java
+++ src/jat/examples/coordinates/comparison/JAT_SPA_compare01.java
@@ -11,61 +11,82 @@ import solarpositioning.SPA;
 
 public class JAT_SPA_compare01 {
 
-	public static void main(final String[] args) {
-		int year = 1980;
-		int month = Calendar.JULY;
-		int day = 27;
-		// int hour = 12;
-		int minute = 0;
-		int second = 0;
-		double latitude = 52.;
-		double longitude = 0.;
-		double elevation = 1830.14;
-		String s, t;
+	public class dataSet {
+		int timeZoneOffset;
+		int year;
+		int month;
+		int day;
+		int hour;
+		int minute;
+		int second;
+		double latitude;
+		double longitude;
 
-		year = 2015;
-		month = Calendar.JANUARY;
-		day = 29;
-		latitude = 30.;
-		longitude=0;
+		public dataSet(int timeZoneOffset, int year, int month, int day, int hour, int minute, int second, double latitude, double longitude, double elevation) {
+
+			this.timeZoneOffset = timeZoneOffset;
+			this.year = year;
+			this.month = month;
+			this.day = day;
+			this.hour = hour;
+			this.minute = minute;
+			this.second = second;
+			this.latitude = latitude;
+			this.longitude = longitude;
+			this.elevation = elevation;
+		}
+
+		double elevation;
+
+	}
+
+	public static void main(final String[] args) {
+		String s, t;
+		JAT_SPA_compare01 jsp = new JAT_SPA_compare01();
+		dataSet ds;
+
+		dataSet Greenwich = jsp.new dataSet(0, 1980, Calendar.JANUARY, 27, 12, 0, 0, 52., 0., 1830.14);
+		dataSet Austin = jsp.new dataSet(-6, 2015, Calendar.JANUARY, 31, 12, 0, 0, 30., -97., 1830.14);
+		ds = Greenwich;
+		ds = Austin;
 
 		// System.out.println(month);
-		System.out.println("SPA vs. JAT");
+		System.out.println("Sun Position SPA vs. JAT");
+		s = String.format("TimeZone %3d date %2d/%2d/%4d lat %-6.2f lon %-6.2f", ds.timeZoneOffset, ds.month+1,ds.day,ds.year,ds.latitude, ds.longitude);
+		System.out.println(s);
 
 		s = String.format("%-4s%-8s%-8s%-10s%-8s%-8s%-8s%-8s%-8s%-8s%-8s", "hr", "GST", "RA", "RA(h)", "dec", "az", "alt", "RA", "dec", "az", "alt");
 		System.out.println(s);
 		for (int hour = 0; hour < 24; hour++) {
 			// System.out.println("hour " + hour);
 
-			// GregorianCalendar time = new GregorianCalendar(new
-			// SimpleTimeZone(0 * 60 * 60 * 1000, "LST"));
-			GregorianCalendar time = new GregorianCalendar(new SimpleTimeZone(SimpleTimeZone.UTC_TIME, "UTC"));
-			time.set(year, month, day, hour, minute, second);
+			GregorianCalendar time = new GregorianCalendar(new SimpleTimeZone(ds.timeZoneOffset * 60 * 60 * 1000, "LST"));
+			time.set(ds.year, ds.month, ds.day, hour, ds.minute, ds.second);
 
-			AzimuthZenithAngle result = SPA.calculateSolarPosition(time, latitude, longitude, elevation, 67, 820, 11);
+			AzimuthZenithAngle result = SPA.calculateSolarPosition(time, ds.latitude, ds.longitude, ds.elevation, 67, 820, 11);
 
 			AstroCoordinate rf = new AstroCoordinate();
 			DateTime epoch = new DateTime(1990, 1, 1, 0, 0, DateTimeZone.forID("UTC"));
-			DateTime currentDateTime = new DateTime(year, month + 1, day, hour, 0);
+			DateTime currentDateTime = new DateTime(ds.year, ds.month + 1, ds.day, hour, 0);
 			rf.sunPosition(epoch, currentDateTime);
 			rf.eclipticToEquatorial(currentDateTime);
 			// // rf.eclipticCoord.println();
 			double rfRA = rf.equatorialCoord.RA.getDegrees();
 			double rfdec = rf.equatorialCoord.dec.getDegrees();
-			rf.equatorialToHorizon(year, month + 1, day, hour, minute, second, longitude, latitude);
+			rf.equatorialToHorizon(ds.year, ds.month + 1, ds.day, hour, ds.minute, ds.second, ds.longitude, ds.latitude);
 			// rf.equatorialCoord.RA.println();
-			//rf.equatorialCoord.println();
+			// rf.equatorialCoord.println();
 			// rf.equatorialToHorizonDS(hour - 3.7, latitude);
 			// rf.horizontalCoord.println();
 
 			double SPAnu = SPA.getNuDegrees();
 			double SPARA = SPA.getAlphaDegrees();
-			//System.out.println(SPARA);
+			// System.out.println(SPARA);
 			double SPARAhdec = SPARA / 15;
 			double tmpminutes = 60. * Frac(SPARAhdec);
 
 			String SPARAh = String.format("%2d:%2d:%2d", (int) SPARAhdec, (int) (60. * Frac(SPARAhdec)), (int) (60. * Frac(tmpminutes)));
-			//System.out.println(SPARAh);
+			// System.out.println(SPARAh);
 			double SPAdec = SPA.getDeltaDegrees();
 			double SPAaz = result.getAzimuth();
 			double SPAalt = 90 - result.getZenithAngle();
