b83227632711d83206b650c4d2e3601d1977fdd0
user <user@asu2>
2015-02-06 00:44:44 -0600

    comparison with Astronomical Almanac

--- src/jat/examples/coordinates/SunPos01.java
+++ src/jat/examples/coordinates/SunPos01.java
@@ -1,5 +1,6 @@
 package jat.examples.coordinates;
 
+import jat.core.coordinates.Angle;
 import jat.core.coordinates.AstroCoordinate;
 import jat.core.coordinates.AstroDateTimeLocation;
 import jat.core.coordinates.solarPositioning.SPA;
@@ -7,11 +8,15 @@ import jat.core.coordinates.solarPositioning.SPA;
 public class SunPos01 {
 	public static void main(String[] args) {
 
-		AstroDateTimeLocation adt = new AstroDateTimeLocation(2015, 2, 2, 13, 0, 0, "CST6CDT");
+		AstroDateTimeLocation adt = new AstroDateTimeLocation(2003, 11, 1, 18, 0, 0, "CST6CDT");
 		AstroCoordinate ac = SPA.getSolarPosition(adt, 30.00, -97.00, 1830.14, 67., 820., 11.);
 		System.out.println(adt.getLocalDateTime());
-		ac.eclipticCoord.println();
+		System.out.println("JD="+adt.getJD());
+
+		ac.eclipticCoord.lambda.println("ecliptic longitude", Angle.ARCDEGREES);
+		ac.eclipticCoord.beta.println("ecliptic latitude", Angle.DEGREES);
 		ac.equatorialCoord.println();
+		//ac.equatorialCoord.println();
 		ac.horizontalCoord.println();
 	}
 
