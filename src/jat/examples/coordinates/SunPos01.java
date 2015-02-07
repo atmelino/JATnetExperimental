f2d40124a49e74cf5e4a27adf7a47218869e9f79
jatnik <jatnik@jat>
2015-02-06 20:54:01 -0600

    sun position

--- src/jat/examples/coordinates/SunPos01.java
+++ src/jat/examples/coordinates/SunPos01.java
@@ -8,7 +8,7 @@ import jat.core.coordinates.solarPositioning.SPA;
 public class SunPos01 {
 	public static void main(String[] args) {
 
-		AstroDateTimeLocation adt = new AstroDateTimeLocation(2003, 11, 1, 18, 0, 0, "CST6CDT");
+		AstroDateTimeLocation adt = new AstroDateTimeLocation(2003, 3, 1, 0, 0, 0, "UTC");
 		AstroCoordinate ac = SPA.getSolarPosition(adt, 30.00, -97.00, 1830.14, 67., 820., 11.);
 		System.out.println(adt.getLocalDateTime());
 		System.out.println("JD="+adt.getJD());
