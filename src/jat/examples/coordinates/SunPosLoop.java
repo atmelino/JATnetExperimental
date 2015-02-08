8ebdc4860a604d98ae1045d764272045b341b6d0
user <user@asu2>
2015-02-07 23:16:01 -0600

    fix JD loop

--- src/jat/examples/coordinates/SunPosLoop.java
+++ src/jat/examples/coordinates/SunPosLoop.java
@@ -13,16 +13,17 @@ public class SunPosLoop {
 
 		Angle longitude = new Angle(-97., Angle.DEGREES);
 		Angle latitude = new Angle(30., Angle.DEGREES);
-		adt = new AstroDateTimeLocation(2003, 3, 1, 0, 0, 0, "UTC", longitude, latitude);
+		adt = new AstroDateTimeLocation(2003, 3, 1, 0, 0, 0, "CST6CDT", longitude, latitude);
 		ac = SPA.getSolarPosition(adt, 1830.14, 67., 820., 11.);
-		System.out.println(adt.getLocalDateTime() + " observer longitude " + longitude.getArcDegString() + " latitude " + latitude.getArcDegString());
+		System.out.println(adt.getLocalDateTime() + " observer longitude " + longitude.getArcDegString() + " latitude "
+				+ latitude.getArcDegString());
 
-		s = String.format("%-12s%-14s%-14s%-14s%-14s%-10s%-10s", "JD", "lambda", "beta", "RA", "dec", "az", "alt");
+		s = String.format("%-12s%-14s%-14s%-14s%-14s%-14s%-14s", "JD", "lambda", "beta", "RA", "dec", "az", "alt");
 		System.out.println(s);
 
-		//for (double increment = 0; increment < 1.; increment += (1/24.0)) {
-			for (double increment = 0; increment < 1.; increment += .1) {
-			adt.advanceJD(increment);
+		// for (double increment = 0; increment < 1.; increment += (1/24.0)) {
+		for (int i = 0; i < 24; i++) {
+			adt.advanceJD(1 / 24.);
 			ac = SPA.getSolarPosition(adt, 1830.14, 67., 820., 11.);
 			printTable(adt, ac);
 		}
@@ -34,8 +35,10 @@ public class SunPosLoop {
 
 		String tf = "%-12.2f%-14s%-14s%-14s%-14s%-14s%-14s";
 
-		t = String.format(tf, adt.getJD(), ac.eclipticCoord.lambda.getArcDegString(), ac.eclipticCoord.beta.getArcDegString(), ac.equatorialCoord.RA.getHAString(),
-				ac.equatorialCoord.dec.getArcDegString(), ac.horizontalCoord.azimuth.getArcDegString(), ac.horizontalCoord.altitude.getArcDegString());
+		t = String.format(tf, adt.getJD(), ac.eclipticCoord.lambda.getArcDegString(),
+				ac.eclipticCoord.beta.getArcDegString(), ac.equatorialCoord.RA.getHAString(),
+				ac.equatorialCoord.dec.getArcDegString(), ac.horizontalCoord.azimuth.getArcDegString(),
+				ac.horizontalCoord.altitude.getArcDegString());
 		System.out.println(t);
 	}
 
