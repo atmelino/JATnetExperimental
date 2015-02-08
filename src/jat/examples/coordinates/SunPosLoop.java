94547ba2fa18ec1310eaf88248c2556d93c3f205
user <user@asu2>
2015-02-07 23:39:10 -0600

    longitude value

--- src/jat/examples/coordinates/SunPosLoop.java
+++ src/jat/examples/coordinates/SunPosLoop.java
@@ -11,23 +11,21 @@ public class SunPosLoop {
 		AstroDateTimeLocation adt;
 		AstroCoordinate ac;
 
-		Angle longitude = new Angle(-97., Angle.DEGREES);
-		Angle latitude = new Angle(30., Angle.DEGREES);
+		Angle longitude = new Angle(-97.733333, Angle.DEGREES);
+		Angle latitude = new Angle(30,17,0, Angle.ARCDEGREES);
 		adt = new AstroDateTimeLocation(2003, 3, 1, 0, 0, 0, "CST6CDT", longitude, latitude);
-		ac = SPA.getSolarPosition(adt, 1830.14, 67., 820., 11.);
-		System.out.println(adt.getLocalDateTime() + " observer longitude " + longitude.getArcDegString() + " latitude "
+
+		System.out.println(adt.getLocalDateTime() + " observer longitude " + longitude.getDegrees() + " latitude "
 				+ latitude.getArcDegString());
 
 		s = String.format("%-12s%-14s%-14s%-14s%-14s%-14s%-14s", "JD", "lambda", "beta", "RA", "dec", "az", "alt");
 		System.out.println(s);
 
-		// for (double increment = 0; increment < 1.; increment += (1/24.0)) {
 		for (int i = 0; i < 24; i++) {
-			adt.advanceJD(1 / 24.);
 			ac = SPA.getSolarPosition(adt, 1830.14, 67., 820., 11.);
+			adt.advanceJD(1 / 24.);
 			printTable(adt, ac);
 		}
-
 	}
 
 	private static void printTable(AstroDateTimeLocation adt, AstroCoordinate ac) {
